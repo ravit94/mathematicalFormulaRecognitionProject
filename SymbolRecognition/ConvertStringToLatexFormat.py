37042a21dc4c97dc4d7e6febd015cd61fb7be1d7
Kobi Nakash <kobi7191@gmail.com>
2018-09-13 20:39:19 +0300

    update the function of convertStringToLatexFormat

--- SymbolRecognition/ConvertStringToLatexFormat.py
+++ SymbolRecognition/ConvertStringToLatexFormat.py
@@ -14,57 +14,14 @@ class ConvertStringToLatexFormat(object):
       :param symbol: recognized by Tesseract or by correlation coefficient
       :type symbol: string
       """
-      if symbol == None:
-          return None
-      if symbol.startswith('alpha'):
-          return '\\alpha'
-      elif symbol.startswith('beta'):
-          return '\\beta'
-      elif symbol.startswith('bigger'):
-          return '>'
-      elif symbol.startswith('dot'):
-          return '\cdot'
-      elif symbol.startswith('epsilon'):
-          return '\epsilon'
-      elif symbol.startswith('phi'):
-          return '\phi'
-      elif symbol.startswith('frac'):
-          return '\\frac'
-      elif symbol.startswith('gamma'):
-          return '\gamma'
-      elif symbol.startswith('infinity'):
-          return '\infty'
-      elif symbol.startswith('integral'):
-          return '\int_'
-      elif symbol.startswith('lambda'):
-          return '\lambda'
-      elif symbol.startswith('leftArrow'):
-          return '\leftarrow'
-      elif symbol.startswith('leftPar'):
-          return '\left ( '
-      elif symbol.startswith('mult'):
-          return 'X'
-      elif symbol.startswith('omega'):
-          return '\omega'
-      elif symbol.startswith('pi'):
-          return '\pi'
-      elif symbol.startswith('rightArrow'):
-          return '\\rightarrow'
-      elif symbol.startswith('rightPar'):
-          return '\\right )'
-      elif symbol.startswith('sig'):
-          return '\sum'
-      elif symbol.startswith('smaller'):
-          return '<'
-      elif symbol.startswith('prod'):
-          return '\prod'
-      elif symbol.startswith('sqrt'):
-          return '\sqrt'
-      elif symbol.startswith('Theta'):
-          return '\Theta'
-      elif symbol.startswith('Unequal'):
-          return '\\neq'
-      elif symbol.startswith('Union'):
-          return '\cup'
+      latexFormat = {'alpha': '\\alpha','beta':'\\beta','bigger':'>','dot':'\cdot','epsilon':'\epsilon',
+       'phi':'\phi','frac':'\\frac','gamma':'\gamma','infinity':'\infty','integral':'\int_','lambda':'\lambda',
+       'leftArrow':'\leftarrow','leftPar':'\left ( ','mult':'X','omega':'\omega','pi':'\pi','rightArrow':'\\rightarrow',
+       'rightPar':'\\right )','sig':'\sum','smaller':'<','prod':'\prod','sqrt':'\sqrt','Theta':'\Theta',
+       'Unequal':'\\neq', 'Union': '\cup', None:None}
+
+      if latexFormat.__contains__(symbol):
+        return latexFormat[symbol]
+
       else:
-          return symbol
+        return symbol
